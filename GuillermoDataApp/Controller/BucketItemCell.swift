311220ce48ae3efbc5d1197b016c391d818e2077
Guillermo <memo33101@gmail.com>
2018-11-30 07:59:48 -0700

    Updated the controller and model folder, and linked classes to storyboard

---
+++ GuillermoDataApp/Controller/BucketItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  BucketItemCell.swift
+//  GuillermoDataApp
+//
+//  Created by Ramos aguirre, Guillermo on 11/30/18.
+//  Copyright © 2018 ctec. All rights reserved.
+//
+
+import UIKit
+
+class BucketItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
