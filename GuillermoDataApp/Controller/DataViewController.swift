b2234b398541fb19572a2cc46d8e2f53339bc92c
Guillermo <memo33101@gmail.com>
2018-11-30 08:56:03 -0700

    Addded a method that loads BucketItem instances from the file into the array

--- GuillermoDataApp/Controller/DataViewController.swift
+++ GuillermoDataApp/Controller/DataViewController.swift
@@ -9,7 +9,35 @@
 import UIKit
 
 class DataViewController: UITableViewController {
-
+    
+    lazy var bucketList : [BucketItem] =
+    {
+        return loadBucketListFromFile()
+    }()
+    
+    private func loadBucketListFromFile() -> [BucketItem]
+    {
+        var items = [BucketItem]()
+        do
+        {
+            if let filePath = Bundle.main.url(forResource: "bucket18", withExtension: "csv")
+            {
+                let input = try String(contentsOf: filePath)
+                let bucketLines = input.components(separatedBy: "\n")
+                for line in bucketLines
+                {
+                    let item = line.components(separatedBy: ",")
+                    items.append(BucketItem(contents: item[0], author: item[1]))
+                }
+            }
+        }
+        catch
+        {
+            print("File load error")
+        }
+        return items
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
